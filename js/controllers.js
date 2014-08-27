fe62572700d9ae1a0289d9d156584d305c54169e
PETER_GAO <petergao@khanacademy.org>
2014-08-27 11:55:41 -0700

    new images, more changes to me, and fix positioning of home elements

--- js/controllers.js
+++ js/controllers.js
@@ -1,40 +1,58 @@
 var homepageApp = angular.module('homepageApp', ['ngRoute', 'ngAnimate']);
 
-homepageApp.config(function($routeProvider) {
+homepageApp.config(function ($routeProvider) {
     $routeProvider
         .when('/', {
             templateUrl: 'html/home.html',
-            controller: 'mainController'
+            controller: 'HomeController',
+            title: 'GAOISM'
         })
 
         .when('/write', {
             templateUrl: 'html/write.html',
-            controller: 'writeController'
+            controller: 'WriteController',
+            title: "I write. | GAOISM"
         })
 
         .when('/make', {
             templateUrl: 'html/make.html',
-            controller: 'makeController'
+            controller: 'MakeController',
+            title: "I make. | GAOISM"
         })
 
         .when('/am', {
             templateUrl: 'html/am.html',
-            controller: 'amController'
+            controller: 'AmController',
+            title: "I am. | GAOISM"
         });
+
 });
 
-homepageApp.controller('mainController', function($scope) {
-    $scope.pageClass = 'page-home'; 
+homepageApp.controller('HeaderController', function ($scope, $location) {
+    $scope.isActive = function (viewLocation) {
+        console.log($location.path());
+        return viewLocation === $location.path();
+    };
 });
 
-homepageApp.controller('writeController', function($scope) {
-    $scope.pageClass = 'page-write'; 
+homepageApp.controller('HomeController', function ($scope) {
+    $scope.pageClass = 'page-home';
 });
 
-homepageApp.controller('makeController', function($scope) {
-    $scope.pageClass = 'page-make'; 
+homepageApp.controller('WriteController', function ($scope) {
+    $scope.pageClass = 'page-write';
 });
 
-homepageApp.controller('amController', function($scope) {
+homepageApp.controller('MakeController', function ($scope) {
+    $scope.pageClass = 'page-make';
+});
+
+homepageApp.controller('AmController', function ($scope) {
     $scope.pageClass = 'page-am'; 
 });
+
+homepageApp.run(['$rootScope', '$location', function ($rootScope, $location) {
+    $rootScope.$on('$routeChangeSuccess', function (event, current, previous) {
+        $rootScope.title = current.$$route.title;
+    });
+}]);
